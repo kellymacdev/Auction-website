dcf614f4607de8885be9a663a9ab6419810fadd5
Kelly MacDevette <kellymacdev@gmail.com>
2025-09-30 16:56:48 +0200

    added an active listings list and linked listing pages

--- auctions/models.py
+++ auctions/models.py
@@ -3,4 +3,21 @@ from django.db import models
 
 
 class User(AbstractUser):
+    #already has fields for username, email. password etc.
+    pass
+
+class Listing(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    starting_bid = models.IntegerField()
+    image_url = models.URLField()
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Listing {self.id}. {self.title}"
+
+class Bid(models.Model):
+    pass
+
+class Comment(models.Model):
     pass
--- auctions/views.py
+++ auctions/views.py
@@ -1,14 +1,17 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
 
-from .models import User
+from .models import User, Listing
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    all_listings = Listing.objects.all()
+    return render(request, "auctions/index.html",{
+        "active_listings": all_listings
+    })
 
 
 def login_view(request):
@@ -61,3 +64,23 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+def listing(request, title):
+    return render(request, "auctions/listing.html", {
+        "item": Listing.objects.get(title=title)
+    })
+
+
+
+
+
+def create_listing(request):
+    if request.method == "POST":
+        title = request.POST["item_title"]
+        description = request.POST["item_description"]
+        starting_bid = request.POST["item_starting_bid"]
+        item_image_url = request.POST["item_image_url"]
+        category = request.POST["category"]
+        return render(request, "auctions/create_listing.html")
+    else:
+        return render(request, "auctions/create_listing.html")
